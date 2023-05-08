2dc00b92507ac269a65f50145bd06933ec8b9c18
scarpart <aenartzr@gmail.com>
2023-05-08 19:00:17 -0300

    Change LB algo: now using Least Connections. Changed logic in general, better subdirectory organization.

--- remote-server/api-server/server.go
+++ remote-server/api-server/server.go
@@ -2,14 +2,9 @@ package apiserver
 
 import (
 	"net"
-	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	db "github.com/scarpart/distributed-task-scheduler/remote-server/db/sqlc"
-	"github.com/shirou/gopsutil/process"
 )
 
 type Server struct {
@@ -33,35 +28,6 @@ func NewServer(store *db.Store, ipAddr net.IP) *Server {
 	router.GET("/node/:node_id", server.GetNode)
 	router.GET("/node", server.GetAllNodes)
 
-	// Prometheus metrics to be read from the main server (Load Balancer)
-	cpuUsage := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "cpu_usage",
-			Help: "CPU usage of a remote server in percentage",
-		},
-		[]string{"endpoint"},
-	)
-	memUsage := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "mem_usage",
-			Help: "Memory usage of a remote server in percentage",
-		},
-		[]string{"endpoint"},
-	)
-	prometheus.MustRegister(cpuUsage, memUsage)
-
-	// GET - Prometheus 
-	router.GET("/metrics", func(ctx *gin.Context) {
-		cpu, mem, err := getUsageStats()
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-			return
-		}
-		cpuUsage.With(prometheus.Labels{"endpoint": server.ipAddr.String()}).Set(cpu)
-		memUsage.With(prometheus.Labels{"endpoint": server.ipAddr.String()}).Set(mem)	
-		promhttp.Handler().ServeHTTP(ctx.Writer, ctx.Request)
-	})
-
 	server.router = router
 	return server
 }
@@ -77,21 +43,3 @@ func errorResponse(err error) gin.H {
 	}
 }
 
-func getUsageStats() (float64, float64, error) {
-	proc, err := process.NewProcess(int32(os.Getpid()))
-	if err != nil {
-		panic("aaa")
-	}
-
-	cpu, err := proc.CPUPercent()
-	if err != nil {
-		return 0, 0, err
-	}
-
-	mem, err := proc.MemoryPercent()
-	if err != nil {
-		return 0, 0, err
-	}
-
-	return cpu, float64(mem), nil
-}
